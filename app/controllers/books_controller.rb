b0e3d0f4081cf9902fe83d65226091467ea74394
kazuma <kazuma@takahashiyumiko-no-MacBook-Pro.local>
2020-09-02 16:42:20 +0900

    appを作ろう

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+
+  def top
+  end
+
+  def show
+   @book = Book.find(params[:id])
+
+  end
+ 
+  def index
+    @book = Book.new
+    @books = Book.all
+
+  end
+
+  def create
+   @book = Book.new(book_params)
+if @book.save
+  flash[:notice] = "Book was successfully created."
+  redirect_to book_path(@book.id)
+else 
+  @books = Book.all
+   render :index
+  end
+end
+ 
+  def edit
+     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    show.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+     book = Book.find(params[:id])
+     book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+     redirect_to books_path
+  end
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+end
+
+end
